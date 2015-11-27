e8b5641a49a4d4dfae036d0ea25fcef12c00334a
Maximilian Riemensberger <riemensberger@tum.de>
2015-11-27 11:35:41 +0100

    cpp: Reformat multicast_sockte.{hpp,cpp} with clang-format

--- src/multicast_socket.cpp
+++ src/multicast_socket.cpp
@@ -7,79 +7,55 @@ using mx3::LoggingMulticastSocketListener;
 using ListenerKey = MulticastSocket::ListenerKey;
 
 MulticastSocket::MulticastSocket(
-        shared_ptr<mx3_gen::MulticastSocket> multicast_socket
-) :
-    multicast_socket_ {move(multicast_socket)},
-    broadcaster_ {make_shared<BroadcastingMulticastSocketListener>()}
-{
-}
+        shared_ptr<mx3_gen::MulticastSocket> multicast_socket)
+        : multicast_socket_{move(multicast_socket)},
+          broadcaster_{make_shared<BroadcastingMulticastSocketListener>()} {}
 
-ListenerKey
-MulticastSocket::add_listener(
-        unique_ptr<mx3_gen::MulticastSocketListener> listener
-) {
+ListenerKey MulticastSocket::add_listener(
+        unique_ptr<mx3_gen::MulticastSocketListener> listener) {
     return broadcaster_->add_listener(move(listener));
 }
 
-bool
-MulticastSocket::del_listener(ListenerKey key)
-{
+bool MulticastSocket::del_listener(ListenerKey key) {
     return broadcaster_->del_listener(key);
 }
 
-void
-MulticastSocket::open(const mx3_gen::SocketAddress & multicast_socket_address)
-{
+void MulticastSocket::open(
+        const mx3_gen::SocketAddress & multicast_socket_address) {
     multicast_socket_->open(multicast_socket_address, broadcaster_);
 }
 
-void MulticastSocket::close()
-{
-    multicast_socket_->close();
-}
+void MulticastSocket::close() { multicast_socket_->close(); }
 
-BroadcastingMulticastSocketListener::BroadcastingMulticastSocketListener() :
-    listeners_ {}
-{
-}
+BroadcastingMulticastSocketListener::BroadcastingMulticastSocketListener()
+        : listeners_{} {}
 
-MulticastSocket::ListenerKey
-BroadcastingMulticastSocketListener::add_listener(
-        unique_ptr<mx3_gen::MulticastSocketListener> listener
-) {
-    ListenerKey key = (ListenerKey) listener.get();
+MulticastSocket::ListenerKey BroadcastingMulticastSocketListener::add_listener(
+        unique_ptr<mx3_gen::MulticastSocketListener> listener) {
+    ListenerKey key = (ListenerKey)listener.get();
     listeners_.insert(std::make_pair(key, move(listener)));
     return key;
 }
 
-bool
-BroadcastingMulticastSocketListener::del_listener(
-        MulticastSocket::ListenerKey key
-) {
+bool BroadcastingMulticastSocketListener::del_listener(
+        MulticastSocket::ListenerKey key) {
     return listeners_.erase(key) > 0;
 }
 
-void
-BroadcastingMulticastSocketListener::on_datagram(const mx3_gen::Datagram & datagram)
-{
+void BroadcastingMulticastSocketListener::on_datagram(
+        const mx3_gen::Datagram & datagram) {
     for (const auto & listener : listeners_)
         listener.second->on_datagram(datagram);
 }
 
 LoggingMulticastSocketListener::LoggingMulticastSocketListener(
-    shared_ptr<mx3_gen::Logger> logger
-) :
-    logger_ {move(logger)}
-{
-}
+        shared_ptr<mx3_gen::Logger> logger)
+        : logger_{move(logger)} {}
 
-void
-LoggingMulticastSocketListener::
-on_datagram(const mx3_gen::Datagram & datagram)
-{
+void LoggingMulticastSocketListener::on_datagram(
+        const mx3_gen::Datagram & datagram) {
     logger_->log(mx3_gen::Logger::LOG_LEVEL_INFO, "MulticastSocket",
-            "DATAGRAM: length = " + datagram.data.size());
+                 "DATAGRAM: length = " + datagram.data.size());
 }
-
 
 /* vim: set et ts=4 sts=4 sw=4 tw=80 : */
--- src/multicast_socket.hpp
+++ src/multicast_socket.hpp
@@ -12,48 +12,49 @@ namespace mx3 {
 class BroadcastingMulticastSocketListener;
 
 class MulticastSocket {
-  public:
+public:
     MulticastSocket(shared_ptr<mx3_gen::MulticastSocket> multicast_socket);
 
     // Listener management
     using ListenerKey = uintptr_t;
-    ListenerKey add_listener(unique_ptr<mx3_gen::MulticastSocketListener> listener);
+    ListenerKey
+    add_listener(unique_ptr<mx3_gen::MulticastSocketListener> listener);
     bool del_listener(ListenerKey key);
 
     // Connection management
     void open(const mx3_gen::SocketAddress & multicast_socket_address);
     void close();
 
-  private:
+private:
     const shared_ptr<mx3_gen::MulticastSocket> multicast_socket_;
     const shared_ptr<mx3::BroadcastingMulticastSocketListener> broadcaster_;
 };
 
-class BroadcastingMulticastSocketListener : public mx3_gen::MulticastSocketListener {
-  public:
+class BroadcastingMulticastSocketListener
+        : public mx3_gen::MulticastSocketListener {
+public:
     BroadcastingMulticastSocketListener();
 
     virtual void on_datagram(const mx3_gen::Datagram & packet) override;
 
     MulticastSocket::ListenerKey
-        add_listener(unique_ptr<mx3_gen::MulticastSocketListener> listener);
+    add_listener(unique_ptr<mx3_gen::MulticastSocketListener> listener);
     bool del_listener(MulticastSocket::ListenerKey key);
 
-  private:
-    std::map<MulticastSocket::ListenerKey, unique_ptr<mx3_gen::MulticastSocketListener>>
-        listeners_;
+private:
+    std::map<MulticastSocket::ListenerKey,
+             unique_ptr<mx3_gen::MulticastSocketListener>> listeners_;
 };
 
 class LoggingMulticastSocketListener : public mx3_gen::MulticastSocketListener {
-  public:
+public:
     LoggingMulticastSocketListener(shared_ptr<mx3_gen::Logger> logger);
 
     virtual void on_datagram(const mx3_gen::Datagram & packet) override;
 
-  private:
+private:
     const shared_ptr<mx3_gen::Logger> logger_;
 };
-
 }
 
 /* vim: set et ts=4 sts=4 sw=4 tw=80 : */
