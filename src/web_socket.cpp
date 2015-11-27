7f4e9347e026f1eb40d9c820613c152d79269e9e
Maximilian Riemensberger <riemensberger@tum.de>
2015-11-27 11:30:21 +0100

    cpp: Reformat web_socket.{hpp,cpp} with clang-format

--- src/web_socket.cpp
+++ src/web_socket.cpp
@@ -7,168 +7,107 @@ using mx3::ForwardingWebSocketListener;
 
 using ListenerKey = WebSocket::ListenerKey;
 
-WebSocket::WebSocket(shared_ptr<mx3_gen::WebSocket> web_socket) :
-    web_socket_ {move(web_socket)},
-    broadcaster_ {make_shared<BroadcastingWebSocketListener>()}
-{
-}
+WebSocket::WebSocket(shared_ptr<mx3_gen::WebSocket> web_socket)
+        : web_socket_{move(web_socket)},
+          broadcaster_{make_shared<BroadcastingWebSocketListener>()} {}
 
 ListenerKey
-WebSocket::add_listener(unique_ptr<mx3_gen::WebSocketListener> listener)
-{
+WebSocket::add_listener(unique_ptr<mx3_gen::WebSocketListener> listener) {
     return broadcaster_->add_listener(move(listener));
 }
 
-bool
-WebSocket::del_listener(ListenerKey key)
-{
+bool WebSocket::del_listener(ListenerKey key) {
     return broadcaster_->del_listener(key);
 }
 
-void
-WebSocket::connect(const string & url)
-{
+void WebSocket::connect(const string & url) {
     web_socket_->connect(url, broadcaster_);
 }
 
-bool WebSocket::is_connected()
-{
-    return web_socket_->is_connected();
-}
+bool WebSocket::is_connected() { return web_socket_->is_connected(); }
 
-void WebSocket::close()
-{
-    web_socket_->close();
-}
+void WebSocket::close() { web_socket_->close(); }
 
-void WebSocket::send(const string & message)
-{
-    web_socket_->send(message);
-}
+void WebSocket::send(const string & message) { web_socket_->send(message); }
 
+BroadcastingWebSocketListener::BroadcastingWebSocketListener() : listeners_{} {}
 
-BroadcastingWebSocketListener::BroadcastingWebSocketListener() :
-    listeners_ {}
-{
-}
-
-ListenerKey
-BroadcastingWebSocketListener::
-add_listener(unique_ptr<mx3_gen::WebSocketListener> listener)
-{
-    ListenerKey key = (ListenerKey) listener.get();
+ListenerKey BroadcastingWebSocketListener::add_listener(
+        unique_ptr<mx3_gen::WebSocketListener> listener) {
+    ListenerKey key = (ListenerKey)listener.get();
     listeners_.insert(std::make_pair(key, move(listener)));
     return key;
 }
 
-bool
-BroadcastingWebSocketListener::
-del_listener(ListenerKey key)
-{
+bool BroadcastingWebSocketListener::del_listener(ListenerKey key) {
     return listeners_.erase(key) > 0;
 }
 
-void
-BroadcastingWebSocketListener::
-on_open(int16_t code, const string & message)
-{
+void BroadcastingWebSocketListener::on_open(int16_t code,
+                                            const string & message) {
     for (const auto & listener : listeners_)
         listener.second->on_open(code, message);
 }
 
-void
-BroadcastingWebSocketListener::
-on_close(int16_t code, const string & message, bool remote)
-{
+void BroadcastingWebSocketListener::on_close(int16_t code,
+                                             const string & message,
+                                             bool remote) {
     for (const auto & listener : listeners_)
         listener.second->on_close(code, message, remote);
 }
 
-void
-BroadcastingWebSocketListener::
-on_error(int16_t code, const string & message)
-{
+void BroadcastingWebSocketListener::on_error(int16_t code,
+                                             const string & message) {
     for (const auto & listener : listeners_)
         listener.second->on_error(code, message);
 }
 
-void
-BroadcastingWebSocketListener::
-on_message(const string & message)
-{
+void BroadcastingWebSocketListener::on_message(const string & message) {
     for (const auto & listener : listeners_)
         listener.second->on_message(message);
 }
 
 ForwardingWebSocketListener::ForwardingWebSocketListener(
-    shared_ptr<mx3_gen::EventListener> listener
-) :
-    listener_ {move(listener)}
-{
-}
+        shared_ptr<mx3_gen::EventListener> listener)
+        : listener_{move(listener)} {}
 
-void
-ForwardingWebSocketListener::
-on_open(int16_t /*code*/, const string & /*message*/)
-{
-}
+void ForwardingWebSocketListener::on_open(int16_t /*code*/,
+                                          const string & /*message*/) {}
 
-void
-ForwardingWebSocketListener::
-on_close(int16_t /*code*/, const string & /*message*/, bool /*remote*/)
-{
-}
+void ForwardingWebSocketListener::on_close(int16_t /*code*/,
+                                           const string & /*message*/,
+                                           bool /*remote*/) {}
 
-void
-ForwardingWebSocketListener::
-on_error(int16_t /*code*/, const string & /*message*/)
-{
-}
+void ForwardingWebSocketListener::on_error(int16_t /*code*/,
+                                           const string & /*message*/) {}
 
-void
-ForwardingWebSocketListener::
-on_message(const string & message)
-{
+void ForwardingWebSocketListener::on_message(const string & message) {
     listener_->on_websocket_message(message);
 }
 
 LoggingWebSocketListener::LoggingWebSocketListener(
-    shared_ptr<mx3_gen::Logger> logger
-) :
-    logger_ {move(logger)}
-{
-}
+        shared_ptr<mx3_gen::Logger> logger)
+        : logger_{move(logger)} {}
 
-void
-LoggingWebSocketListener::
-on_open(int16_t code, const string & message)
-{
+void LoggingWebSocketListener::on_open(int16_t code, const string & message) {
     logger_->log(mx3_gen::Logger::LOG_LEVEL_INFO, "WebSocket",
-            "OPEN Code: " + to_string(code) + " Message: " + message);
+                 "OPEN Code: " + to_string(code) + " Message: " + message);
 }
 
-void
-LoggingWebSocketListener::
-on_close(int16_t code, const string & message, bool /*remote*/)
-{
+void LoggingWebSocketListener::on_close(int16_t code, const string & message,
+                                        bool /*remote*/) {
     logger_->log(mx3_gen::Logger::LOG_LEVEL_INFO, "WebSocket",
-            "CLOSE Code: " + to_string(code) + " Message: " + message);
+                 "CLOSE Code: " + to_string(code) + " Message: " + message);
 }
 
-void
-LoggingWebSocketListener::
-on_error(int16_t code, const string & message)
-{
+void LoggingWebSocketListener::on_error(int16_t code, const string & message) {
     logger_->log(mx3_gen::Logger::LOG_LEVEL_ERROR, "WebSocket",
-            "ERROR Code: " + to_string(code) + " Message: " + message);
+                 "ERROR Code: " + to_string(code) + " Message: " + message);
 }
 
-void
-LoggingWebSocketListener::
-on_message(const string & message)
-{
+void LoggingWebSocketListener::on_message(const string & message) {
     logger_->log(mx3_gen::Logger::LOG_LEVEL_INFO, "WebSocket",
-            "MESSAGE: " + message);
+                 "MESSAGE: " + message);
 }
 
 /* vim: set et ts=4 sts=4 sw=4 tw=80 : */
--- src/web_socket.hpp
+++ src/web_socket.hpp
@@ -16,11 +16,12 @@ class BroadcastingWebSocketListener;
  * WebSocketListener.
  */
 class WebSocket {
-  public:
+public:
+    using ListenerKey = uintptr_t;
+
     WebSocket(shared_ptr<mx3_gen::WebSocket> web_socket);
 
     // Listener management
-    using ListenerKey = uintptr_t;
     ListenerKey add_listener(unique_ptr<mx3_gen::WebSocketListener> listener);
     bool del_listener(ListenerKey key);
 
@@ -32,57 +33,59 @@ class WebSocket {
     // Message transmission
     void send(const string & message);
 
-  private:
+private:
     // Handle to the WebSocket interface
     const shared_ptr<mx3_gen::WebSocket> web_socket_;
-    // WebSocketListener interface handle that is passed to the WebSocket interface
+    // WebSocketListener interface handle that is passed to web_socket_
     const shared_ptr<mx3::BroadcastingWebSocketListener> broadcaster_;
 };
 
 class BroadcastingWebSocketListener : public mx3_gen::WebSocketListener {
-  public:
+public:
+    using ListenerKey = WebSocket::ListenerKey;
+
     BroadcastingWebSocketListener();
 
     virtual void on_open(int16_t code, const string & message) override;
-    virtual void on_close(int16_t code, const string & message, bool remote) override;
+    virtual void on_close(int16_t code, const string & message,
+                          bool remote) override;
     virtual void on_error(int16_t code, const string & message) override;
     virtual void on_message(const string & message) override;
 
-    WebSocket::ListenerKey
-        add_listener(unique_ptr<mx3_gen::WebSocketListener> listener);
-    bool del_listener(WebSocket::ListenerKey key);
+    ListenerKey add_listener(unique_ptr<mx3_gen::WebSocketListener> listener);
+    bool del_listener(ListenerKey key);
 
-  private:
-    std::map<WebSocket::ListenerKey, unique_ptr<mx3_gen::WebSocketListener>>
-        listeners_;
+private:
+    std::map<ListenerKey, unique_ptr<mx3_gen::WebSocketListener>> listeners_;
 };
 
 class ForwardingWebSocketListener : public mx3_gen::WebSocketListener {
-  public:
+public:
     ForwardingWebSocketListener(shared_ptr<mx3_gen::EventListener> Listener);
 
     virtual void on_open(int16_t code, const string & message) override;
-    virtual void on_close(int16_t code, const string & message, bool remote) override;
+    virtual void on_close(int16_t code, const string & message,
+                          bool remote) override;
     virtual void on_error(int16_t code, const string & message) override;
     virtual void on_message(const string & message) override;
 
-  private:
+private:
     const shared_ptr<mx3_gen::EventListener> listener_;
 };
 
 class LoggingWebSocketListener : public mx3_gen::WebSocketListener {
-  public:
+public:
     LoggingWebSocketListener(shared_ptr<mx3_gen::Logger> logger);
 
     virtual void on_open(int16_t code, const string & message) override;
-    virtual void on_close(int16_t code, const string & message, bool remote) override;
+    virtual void on_close(int16_t code, const string & message,
+                          bool remote) override;
     virtual void on_error(int16_t code, const string & message) override;
     virtual void on_message(const string & message) override;
 
-  private:
+private:
     const shared_ptr<mx3_gen::Logger> logger_;
 };
-
 }
 
 /* vim: set et ts=4 sts=4 sw=4 tw=80 : */
